03e86589e5ad901646d68204a7a5f34e75221306
Nektarium <shmorosoff@gmail.com>
2025-02-01 17:05:39 +0700

    Заполнение моделей

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'chat_id', 
+        'content', 
+        'is_from_guest'
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
 }
